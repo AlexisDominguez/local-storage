403c7245bf41c3b4c9e108b0ff9620cb3240d6fd
AlexisDominguez <alexisarieldominguezsanchez@pop-os.localdomain>
2020-01-09 20:22:37 -0700

    Carga de Local Storage finalizado

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,6 +17,9 @@ document.getElementById("formulario").addEventListener("submit", function(e){
    //Añadir elementos al DOM
    lista.appendChild(botonBorrar);
    listaTweets.appendChild(lista);
+
+   //Añadir tweet a Local Storage
+   agregarTweetLocalStorage(tweet);
 });
 
 //Función para elimitar tweets
@@ -27,4 +30,45 @@ document.getElementById("lista-tweets").addEventListener("click", function(e){
       e.target.parentElement.remove();
    }
 
+});
+
+//Función para agregar tweets a Local Storage
+function agregarTweetLocalStorage(tweet){
+   let tweets;
+    //Activar función para obtener tweets y guardar nuevos
+   tweets = obtenerTweetsLocalStorage()
+   tweets.push(tweet);
+   //Convertir arreglo a string para que JSON lo pueda leer
+   localStorage.setItem("tweets", JSON.stringify(tweets));
+}
+
+//Función para leer/obtener los tweets
+function obtenerTweetsLocalStorage(){
+   let tweets;
+   if(localStorage.getItem("tweets") === null){
+      tweets = [];
+   }else{
+      //Conversión del arreglo a JSON
+      tweets = JSON.parse(localStorage.getItem("tweets"));
+   }
+   return tweets;
+}
+
+//Función para cargar los tweets del local storage
+document.addEventListener("DOMContentLoaded", function(event) {
+   let tweets;
+   tweets = obtenerTweetsLocalStorage();
+   tweets.forEach(function(tweet){
+      const lista = document.createElement("li");
+      lista.innerText = tweet;
+
+      //Añadir botón "borrar"
+      const botonBorrar = document.createElement("a");
+      botonBorrar.innerText = "X";
+      botonBorrar.classList = "borrar-tweet";
+
+      //Añadir elementos al DOM
+      lista.appendChild(botonBorrar);
+      listaTweets.appendChild(lista);
+   })
 });
